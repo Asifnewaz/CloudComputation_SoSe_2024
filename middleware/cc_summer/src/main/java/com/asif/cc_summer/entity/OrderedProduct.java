60b13e8150ed77926aa7324edad303c9ee014e54
Asif Newaz <asifnewaz.cste@gmail.com>
2024-07-22 16:43:38 +0200

    add to card api added

--- middleware/cc_summer/src/main/java/com/asif/cc_summer/entity/OrderedProduct.java
+++ middleware/cc_summer/src/main/java/com/asif/cc_summer/entity/OrderedProduct.java
@@ -1,11 +1,18 @@
 package com.asif.cc_summer.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
-public class OrderedProduct extends BaseClass {
+public class OrderedProduct  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private Integer productID;
     private Integer userID;
 }
