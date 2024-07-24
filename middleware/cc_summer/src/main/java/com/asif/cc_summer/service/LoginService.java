9ff447bf12ba25220eceeba57d774933a3d86394
SaddamHossain2 <103891920+SaddamHossain2@users.noreply.github.com>
2024-07-24 19:04:30 +0200

    login panel done

--- middleware/cc_summer/src/main/java/com/asif/cc_summer/service/LoginService.java
+++ middleware/cc_summer/src/main/java/com/asif/cc_summer/service/LoginService.java
@@ -1,17 +1,33 @@
 package com.asif.cc_summer.service;
 
+import com.asif.cc_summer.entity.Cart;
+import com.asif.cc_summer.entity.UserEntity;
+import com.asif.cc_summer.repository.CategoryRepository;
+import com.asif.cc_summer.repository.LoginRepository;
+import com.mysql.cj.callback.UsernameCallback;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+    private final LoginRepository loginRepository;
+
     private static final String CORRECT_USERNAME = "admin";
     private static final String CORRECT_PASSWORD = "password123";
 
-    public String verifyLogin(String Username, String Password){
-        if (CORRECT_USERNAME.equals(Username) && CORRECT_PASSWORD.equals(Password)) {
-            return "Login successful!";
-        } else {
-            return "Invalid username or password. Login failed.";
+    public String verifyLogin(String Username, String Password) {
+
+        List<UserEntity> allData = loginRepository.findAll();
+        for (int i = 0; i < allData.size(); i++) {
+            UserEntity login =  allData.get(i);
+            if (login.getUser_name().equals(Username)) {
+                return "Login successful!";
+            }
         }
+
+        return "Invalid username or password. Login failed.";
     }
-}
+    }
