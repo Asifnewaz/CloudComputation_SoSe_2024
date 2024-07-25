50faefbe27cc5d13e238832de189a9402e3298f2
Mazba Uddin <mazba09@gmail.com>
2024-07-26 01:46:10 +0200

    pages going to dynamic

--- webshop/src/app/my-cart/my-cart.component.ts
+++ webshop/src/app/my-cart/my-cart.component.ts
@@ -1,14 +1,48 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { DataServiceService } from '../services/data-service.service';
+import { SessionServiceService } from '../services/session-service.service';
+import { LoadingComponent } from '../loading/loading.component';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 @Component({
   selector: 'app-my-cart',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgFor],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgFor, LoadingComponent],
   templateUrl: './my-cart.component.html',
   styleUrl: './my-cart.component.css'
 })
-export class MyCartComponent {
+export class MyCartComponent implements OnInit {
+  public isLoading: boolean = false;
+  public allProductInMyCart: any = [];
+  public totalPrice: number = 0;
 
+  constructor(private dataService: DataServiceService, private session: SessionServiceService, private cartService: ShoppingCartService) { }
+
+  ngOnInit(): void {
+    this.fetchAllProductInMyCart();
+  }
+
+  fetchAllProductInMyCart() {
+    var url = 'cartList';
+
+    var userID = {
+      userID: this.session.getAccessToken()
+    }
+
+    this.dataService.postData(url, false, userID).subscribe((response) => {
+      this.isLoading = true;
+
+      if (response) {
+        this.isLoading = false;
+        if (response.body.status == 200) {
+          console.log(response);
+          this.allProductInMyCart = response.body.data;
+          this.totalPrice = this.cartService.getTotalPrice(this.allProductInMyCart);
+          console.log(this.totalPrice);
+        }
+      }
+    });
+  }
 }
