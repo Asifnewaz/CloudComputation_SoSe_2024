195cfd02447b163d192c7a55858364612784e1bf
Asif Newaz <asifnewaz.cste@gmail.com>
2024-07-27 15:57:00 +0200

    Product API updated

--- middleware/cc_summer/src/main/java/com/asif/cc_summer/controllers/ProductController.java
+++ middleware/cc_summer/src/main/java/com/asif/cc_summer/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.asif.cc_summer.controllers;
 
 import com.asif.cc_summer.dto.request.ProductDto;
+import com.asif.cc_summer.dto.response.BaseResponseDto;
 import com.asif.cc_summer.entity.Product;
 import com.asif.cc_summer.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -8,24 +9,47 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/product")
 public class ProductController {
     private final ProductService productService;
 
-
     @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> saveProduct(@RequestBody ProductDto product) {
+    public ResponseEntity<?> saveProduct(@RequestParam ProductDto product) {
+        Product obj = productService.saveProduct(product);
 
-        Product response = productService.saveProduct(product);
+        BaseResponseDto response = new BaseResponseDto();
+        response.statusCode = 200;
+        response.success_message = "Category added successfully";
+        response.data = obj;
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/productWithID", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> productWithID(@RequestParam Integer productID) {
+
+        Product obj = productService.getProductById(Long.valueOf(productID));
+        BaseResponseDto response = new BaseResponseDto();
+        response.statusCode = 200;
+        response.success_message = "Category added successfully";
+        response.data = obj;
+
         return ResponseEntity.ok(response);
     }
 
     //get product
     @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getProduct() {
-        return ResponseEntity.ok(productService.getAllProducts());
+        List<Product> products = productService.getAllProducts();
+        BaseResponseDto response = new BaseResponseDto();
+        response.statusCode = 200;
+        response.success_message = "Category added successfully";
+        response.data = products;
+        return ResponseEntity.ok(response);
     }
+
 
 }
